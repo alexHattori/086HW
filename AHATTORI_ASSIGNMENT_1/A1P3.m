5b8d6ac83ad1a3110fb05867df75a16ea5d04884
alexHattori <Alex@dhcp-18-111-102-141.dyn.mit.edu>
2016-09-12 18:32:13 -0400

    Update

--- AHATTORI_ASSIGNMENT_1/A1P3.m
+++ AHATTORI_ASSIGNMENT_1/A1P3.m
@@ -1,7 +1,17 @@
 P = 0;
 for i = 0:n
-    P=P+(-1)^i*x^(2*i+1)/factorial(2*i+1);
+    if mod(i,2)==1
+        val = (x^i)/factorial(i);
+        if mod(i,3) ==0 || mod(i,2)==0
+            val = -val;
+        end
+        P = P+val;
+    end
+    
 end
 
-comment3b = 'The larger n is, the more accurate P will be to the actual value';
+comment3b = 'The larger n is, the more accurate P (our calculated approximation) will be to the actual value';
+
+comment3c = 'Error when n = 1 and x = pi/4 is -.0783. Upper bound for the error is .2181. Greater values for n will decrease error.';
+
 
