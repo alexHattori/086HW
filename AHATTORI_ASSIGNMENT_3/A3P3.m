119316018e65ed9dd2f8e4918630b3a38f7f38d9
alexHattori <ahattori@mit.edu>
2016-10-04 21:37:22 -0400

    Finished A3

--- AHATTORI_ASSIGNMENT_3/A3P3.m
+++ AHATTORI_ASSIGNMENT_3/A3P3.m
@@ -1,11 +1,13 @@
 h = 0.01;
-x = 0:h:3.5;
+x = 0:h:3.5-h;
 y = x.^2.*sin(pi*x);
 Int_3a = 0;
 for i = 1:length(x)-1
     Int_3a = Int_3a+h*y(i+1);
 end
 
+x = 0:h:3.5;
+y = x.^2.*sin(pi*x);
 Int_3b = 0;
 for i = 1:length(x)-1
     Int_3b = Int_3b+h*(y(i)+y(i+1))/2;
@@ -18,22 +20,29 @@ Int_exact = -0.773751354362928;
 
 for i = 1:length(h)
     step = h(i);
-    x = 0:step:3.5;
-    y = x.^2.*sin(pi*x);
+    x = 0:step:3.5-step;
+    y = (x.^2).*sin(pi*x);
     
     left = 0;
     for j = 1:length(x)-1
         left = left+step*y(j+1);
     end
-    lefts(i) = left-Int_exact;
+    lefts(i) = abs(left-Int_exact);
+    
+    x = 0:step:3.5;
+    y = (x.^2).*sin(pi*x);
     trap = 0;
     for j = 1:length(x)-1
         trap = trap+step*(y(j)+y(j+1))/2;
     end
-    traps(i) = trap-Int_exact;
+    traps(i) = abs(trap-Int_exact);
 end
 
 figure;
 title('A3P3c');
-plot(1./h,loglog(lefts),'r*',1./h,loglog(traps),'b*');
+loglog(1./h,lefts,'r*-',1./h,traps,'b*-');
+xlabel('log(1/h)');
+ylabel('log(Error)');
+title('Error vs Step Size');
+grid on;
 legend('Left Estimate','Trapezoidal Estimate');
