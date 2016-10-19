c06fc317e5183246f42efd81e69ff658cd8e4736
alexHattori <Alex@dhcp-18-111-106-63.dyn.mit.edu>
2016-10-18 22:06:56 -0400

    A5 COmmit

---
+++ AHATTORI_ASSIGNMENT_5/A5P2.m
@@ -0,0 +1,17 @@
+theta1 = pi/4;
+theta2 = 3*pi/4;
+l1 = 0.3;
+l2 = 0.3;
+
+Js = [-l1*sin(theta1) -l2*sin(theta2);l1*cos(theta1) l2*cos(theta2)];
+
+T = [1 0; 1 1];
+
+Jr = Js*T;
+
+test = [Jr [-1;1]];
+
+rref(test);
+
+CommentA5P2d = 'It does not exist';
+CommentA5P2e = 'It does exist';
