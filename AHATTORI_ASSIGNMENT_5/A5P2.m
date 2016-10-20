917fb9efd39d1777f5e5d4492ae3a9206e838ce4
alexHattori <Alex@dhcp-18-111-116-71.dyn.mit.edu>
2016-10-20 14:25:24 -0400

    A5 SemiFinal

--- AHATTORI_ASSIGNMENT_5/A5P2.m
+++ AHATTORI_ASSIGNMENT_5/A5P2.m
@@ -1,5 +1,5 @@
 theta1 = pi/4;
-theta2 = pi/4;
+theta2 = 3*pi/4;
 l1 = 0.3;
 l2 = 0.3;
 
@@ -7,11 +7,7 @@ Js = [-l1*sin(theta1) -l2*sin(theta2);l1*cos(theta1) l2*cos(theta2)];
 
 T = [1 0; 1 1];
 
-Jr = Js*T;
-
-test = [Jr [1;1]];
-
-rref(test);
+Jr = double(Js*T);
 
 CommentA5P2d = 'It does not exist';
-CommentA5P2e = 'It does exist';
+CommentA5P2e = 'Infinite Solutions exist';
