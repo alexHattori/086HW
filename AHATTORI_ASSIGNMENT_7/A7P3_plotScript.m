e937cb38fb749f854e5306a42faeb2c8d09c88be
alexHattori <Alex@dhcp-18-111-40-142.dyn.mit.edu>
2016-11-03 13:56:00 -0400

    Working on 3c

--- AHATTORI_ASSIGNMENT_7/A7P3_plotScript.m
+++ AHATTORI_ASSIGNMENT_7/A7P3_plotScript.m
@@ -19,16 +19,16 @@ n = size(x,1);% length of any of the position elements
 figure(1);clf;hold off;
 
 subplot(221) ; plot(x,y,'k.-',x(1),y(1),'ko',x(n),y(n),'k*'); 
-axis(a); title('True x,y [ o:start, *:end) ]'); ylabel('North, m');
+ title('True x,y [ o:start, *:end) ]'); ylabel('North, m');
 
 subplot(222) ; plot(xm,ym,'r.-',x(1),y(1),'ko',x(n),y(n),'k*'); 
-axis(a);title('Measured x,y');
+title('Measured x,y');
 
 subplot(223) ; plot(xdr,ydr,'r.-',x(1),y(1),'ko',x(n),y(n),'k*');
-axis(a);title('Dead-reckoned x,y');xlabel('East, m');ylabel('North, m');
+title('Dead-reckoned x,y');xlabel('East, m');ylabel('North, m');
 
 subplot(224) ; plot(xf,yf,'.-',x(1),y(1),'ko',x(n),y(n),'k*');
-axis(a);title('Fusion x,y'); xlabel('East, m');
+title('Fusion x,y'); xlabel('East, m');
 
 disp(sprintf('True x,y start and end positions:  [%g,%g], [%g,%g]',...
     x(1),y(1),x(end),y(end)));
